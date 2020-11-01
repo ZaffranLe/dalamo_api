9e898b43f856f602bc84b6a6df3a74bfd0fa83ce
bangboong1106 <nguyenbangsmile1999@gmail.com>
2020-11-01 20:35:04 +0700

    sưa api

--- app/Http/Controllers/Api/Client/ProductController.php
+++ app/Http/Controllers/Api/Client/ProductController.php
@@ -19,25 +19,39 @@ class ProductController extends Controller
     {
 
         $product = DB::table('product')
-                // ->leftJoin('property','product.id','=','property.idProduct')
+                ->leftJoin('property','product.id','=','property.idProduct')
                 ->leftJoin('brand','brand.id','=','product.idBrand')
                 ->leftJoin('category','category.id','=','product.idCategory')
                 ->leftJoin('image','image.idProduct','=','product.id')
                 ->leftJoin('comment','comment.idProduct','=','product.id')
-                ->select('product.id','brand.name as brandName','product.name','product.price','product.description'
-                ,'product.characteristic','product.guide','product.ingredient','product.preservation','product.origin'
-                ,'product.storageQuantity','product.transportingQuantity','product.isDiscount',
-                'product.discountPercent','product.isHot','product.isNew','image.id as id_img','image.name as name_img',
-                'category.name as category_Name','category.slug as category_Slug','category.id as category_Id')
+                ->select('product.id','brand.name as brandName','product.name','product.price',
+                'product.description','product.characteristic','product.guide','product.ingredient',
+                'product.preservation','product.origin','product.storageQuantity',
+                'product.transportingQuantity','product.isDiscount','product.discountPercent',
+                'product.isHot','product.isNew','image.id as id_img','image.name as name_img',
+                'category.name as category_Name','property.id as id_property',
+                'property.name as name_property','property.value as value_property')
                 ->get();
                 $productList=[];
                 foreach ($product as $val) {
                     if(isset($productList[$val->id])){
                             //Ton tai
-                            $productList[$val->id]['images'][] =  [
+                            $productList[$val->id]
+                            ['images'][] =[
                                 'id_img'=> $val->id_img,
                                 'name_img'=> $val->name_img
                             ];
+                            $productList[$val->id]
+                            ['property'][] =[
+                                'id_property'=>$val->id_property,
+                                 'name_property'=>$val->name_property,
+                                'value_property'=>$val->value_property
+                            ];
+                        //     ['property'][]=[
+                        //         'id_property'=>$val->id_property,
+                        //         'name_property'=>$val->name_property,
+                        //         'value_property'=>$val->value_property
+                        // ];
                             // $productList[]=$item;
                     }else{
                             //Chua ton tai
@@ -57,10 +71,18 @@ class ProductController extends Controller
                                 'discountPercent'=>$val->discountPercent,
                                 'isHot'=>$val->isHot,
                                 'isNew'=>$val->isNew,
+                                'category_Name'=>$val->category_Name,
+                                'brandName'=>$val->brandName,
                                 'images'=> [
                                     ['id_img'=> $val->id_img,
                                     'name_img'=> $val->name_img]
-                                ]
+                                ],
+                                'property' => [
+                                    [
+                                    'id_property'=>$val->id_property,
+                                    'name_property'=>$val->name_property,
+                                    'value_property'=>$val->value_property]
+                                    ]
                                 ];
                             $productList[$val->id] =$item;
                     }
--- routes/api.php
+++ routes/api.php
@@ -17,32 +17,32 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResource('client/brand', 'Api\Client\BrandController');
+Route::apiResource('admin/brand', 'Api\Admin\BrandController');
 
-Route::apiResource('client/category', 'Api\Client\CategoryController');
+Route::apiResource('admin/category', 'Api\Admin\CategoryController');
 
-Route::apiResource('client/comment', 'Api\Client\CommentController');
+Route::apiResource('admin/comment', 'Api\Admin\CommentController');
 
-Route::apiResource('client/detail-import-product', 'Api\Client\DIPController');
+Route::apiResource('admin/detail-import-product', 'Api\Admin\DIPController');
 
-Route::apiResource('client/detail-order', 'Api\Client\DOController');
+Route::apiResource('admin/detail-order', 'Api\Admin\DOController');
 
-Route::apiResource('client/import-product', 'Api\Client\IPController');
+Route::apiResource('admin/import-product', 'Api\Admin\IPController');
 
-Route::apiResource('client/order-receipt', 'Api\Client\ORController');
+Route::apiResource('admin/order-receipt', 'Api\Admin\ORController');
 
-Route::apiResource('client/order-status', 'Api\Client\OSController');
+Route::apiResource('admin/order-status', 'Api\Admin\OSController');
 
-Route::apiResource('client/product', 'Api\Client\ProductController');
+Route::apiResource('admin/product', 'Api\Admin\ProductController');
 
-Route::apiResource('client/provider', 'Api\Client\ProviderController');
+Route::apiResource('admin/provider', 'Api\Admin\ProviderController');
 
-Route::apiResource('client/role', 'Api\Client\RoleController');
+Route::apiResource('admin/role', 'Api\Admin\RoleController');
 
-Route::apiResource('client/user', 'Api\Client\UserController');
+Route::apiResource('admin/user', 'Api\Admin\UserController');
 
-Route::apiResource('client/image', 'Api\Client\ImageController');
+Route::apiResource('admin/image', 'Api\Admin\ImageController');
 
-Route::apiResource('client/property', 'Api\Client\PropertyController');
+Route::apiResource('admin/property', 'Api\Admin\PropertyController');
 
 
