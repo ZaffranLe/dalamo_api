1bf089a16e61ce2693efe83cb1e1bc1417d6c579
Zaffran Le <biprono12@gmail.com>
2020-11-24 23:34:24 +0700

    add fillable

--- app/Models/OR.php
+++ app/Models/OR.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Order_receipt extends Model
 {
     protected $table='order_receipt';
-    protected $fillable=['address','idStatus','phone','name','createdBy','createdDate','isDeleted'];
+    protected $fillable=['address','idStatus','phone','name','createdBy','createdDate','isDeleted', 'idUser', 'totalPrice'];
     public $timestamps=false;
 }
